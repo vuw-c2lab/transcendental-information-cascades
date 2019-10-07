91042f690128bc84857d351f93935331af514760
m-l-r <accounts@markus-luczak.de>
2019-10-08 07:54:33 +1300

    two more POS tokenisers (VERB and ADJ)

--- src/createTIC/tokenisers/text/POS-verbs/tokeniser.R
+++ src/createTIC/tokenisers/text/POS-verbs/tokeniser.R
@@ -1,17 +1,17 @@
 # MAIN FUNCTION -----------------------------------------------------------
 
-# NLP init
-
-# download
-#model <- udpipe::udpipe_download_model(language = "english")
-#model <- udpipe::udpipe_load_model(file = model$file_model)
-
-# or load from file 
-model <- udpipe::udpipe_load_model(file = "/Users/MLR/english-ewt-ud-2.4-190531.udpipe") #english
-#model <- udpipe::udpipe_load_model(file = "/Users/MLR/indonesian-gsd-ud-2.4-190531.udpipe") #bahasa
-#model <- udpipe::udpipe_load_model(file = "/Users/MLR/portuguese-bosque-ud-2.4-190531.udpipe") #portuguese
-
 tokenise <- function(dataSource){
+  # NLP init
+  
+  # download
+  #model <- udpipe::udpipe_download_model(language = "english")
+  #model <- udpipe::udpipe_load_model(file = model$file_model)
+  
+  # or load from file 
+  model <- udpipe::udpipe_load_model(file = "/Users/MLR/english-ewt-ud-2.4-190531.udpipe") #english
+  #model <- udpipe::udpipe_load_model(file = "/Users/MLR/indonesian-gsd-ud-2.4-190531.udpipe") #bahasa
+  #model <- udpipe::udpipe_load_model(file = "/Users/MLR/portuguese-bosque-ud-2.4-190531.udpipe") #portuguese
+  
   # read source data
   sourceData <- readr::read_csv(paste0(getwd(),"/data/",dataSource), col_names = F)
   sourceData$X1 <- tolower(sourceData$X1)
